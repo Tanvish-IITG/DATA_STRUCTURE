1d7ce272fa7a51c978549fdb522eff56d95dd4da
Tanvish <51182758+Tanvish-IITG@users.noreply.github.com>
2019-07-30 12:13:58 +0530

    Add files via upload

---
+++ lab1/add.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> add(int a,int b, int c)
+{
+    vector<int> v = {(a+b+c)%10,(a+b+c)/10};
+    return v;
+}
+
+deque<int> addnum(deque<int> a,deque<int> b)
+{
+    int la = int(a.size());
+    int lb = int(b.size());;
+     int l = max(la,lb);
+    
+    if(l == la)
+    {
+        for(int i = 0;i<(l-lb);i++)
+        {
+            b.push_front(0);
+        }
+    }
+    else
+    {
+        for(int i = 0;i<(l-la);i++)
+        {
+            a.push_front(0);
+        }
+    }
+    int c = 0;
+    deque<int> ans;
+    for(int i = l-1;i>=0;i--)
+    {   
+        
+        vector<int> v = add(a[i],b[i],c);
+        ans.push_back(v[0]);
+        c = v[1];
+        
+    }
+    if(c!=0)
+    {
+        ans.push_back(c);
+    }
+    reverse(begin(ans),end(ans));
+    return ans;
+
+   
+}
+
+deque<int> input()
+{
+    deque<int> num;
+    string s;
+    cin>>s;
+    int i = 0;
+    while(s[i]!='\0')
+    {
+        num.push_back(s[i]-'0');
+        i++;
+    }
+    return num;
+
+
+}
+
+void display(deque<int> v)
+{
+    for(auto X:v)
+    {
+        cout<<X;
+    }
+    cout<<"\n";
+}
+
+int main(void)
+{
+    deque<int> a = input();
+    deque<int> b = input();
+    //display(a);
+    //display(b);
+    deque<int> c = addnum(a,b);
+    display(c);
+    return 0;
+
+}
