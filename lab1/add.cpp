b0eaf38c48305ba90b809f0a8c7e5b56b66cb28b
Tanvish <51182758+Tanvish-IITG@users.noreply.github.com>
2019-08-04 12:14:46 +0530

    Update add.cpp

--- lab1/add.cpp
+++ lab1/add.cpp
@@ -74,13 +74,17 @@ void display(deque<int> v)
 }
 
 int main(void)
-{
+{   int n;
+    cin>>n;
+    for(int i = 0;i<n;i++)
+    {
     deque<int> a = input();
     deque<int> b = input();
     //display(a);
     //display(b);
     deque<int> c = addnum(a,b);
     display(c);
+    }
     return 0;
 
 }
