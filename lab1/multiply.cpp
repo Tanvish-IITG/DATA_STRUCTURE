28f66150e816f1544c41577d01454f3ca3061191
Tanvish <51182758+Tanvish-IITG@users.noreply.github.com>
2019-08-04 12:21:51 +0530

    Update multiply.cpp

--- lab1/multiply.cpp
+++ lab1/multiply.cpp
@@ -115,12 +115,20 @@ deque<int> multnum(deque<int> a,deque<int> b)
 
 int main(void)
 {
+    int n;
+
+    cin>>n;
+
+    for(int i = 0;i<n;i++)
+
+    {
     deque<int> a = input();
     deque<int> b = input();
     //display(a);
     //display(b);
     deque<int> c = multnum(a,b);
     display(c);
+    }
     return 0;
 
 }
